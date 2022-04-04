ed836b0ecc71fbf851e586d072bce1d5cbe3ba5d
Rizki Putra Aulia <rputraaulia@gmail.com>
2022-04-04 21:21:01 +0700

    select

--- src/App.js
+++ src/App.js
@@ -1,104 +1,98 @@
-
 import React, { useState, useEffect } from 'react';
-import './index.css';
+import './index.js';
 import Song from './components/song';
 
 const axios = require('axios');
 
 function App() {
 
-  const [access_token, set_access_token] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
   const [search, set_search] = useState('');
   const [tracks, set_tracks] = useState([]);
+  const [selected, setSelected] = useState([]);
+  const [combineTrack, setCombineTrack] = useState([]);
+  
+  var client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  var scope = 'playlist-modify-private';
+  var redirect_uri = 'http://localhost:3000';
 
-  const LoginButton = () => {
+  var state = "Spotify-React"
 
-    let client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-    let scope = 'playlist-modify-private';
-    let redirect_uri = 'http://localhost:3000';
+  localStorage.setItem("TOKEN", state)
 
-    let spotify_url = 'https://accounts.spotify.com/authorize';
-        spotify_url += '?response_type=token';
-        spotify_url += '&client_id=' + encodeURIComponent(client_id);
-        spotify_url += '&scope=' + encodeURIComponent(scope);
-        spotify_url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-
-    return (
-      <a href={spotify_url} className="login">
-        Login Spotify
-      </a>
-    );
-  }
-
-  const Form = () => {
-    return (
-      <div className="search-bar">
-        <input value={search} onChange={e => set_search(e.target.value)} placeholder="Search" />
-        <button onClick={() => {handleClick()}} className="button-login">Search</button>
-      </div>
-    );
-  }
-
-  function handleClick() {
-    try {
-      let url = 'https://api.spotify.com/v1/search?q='+search+'&type=track,artist';
-      axios.get(url, {
-        headers: {
-          'Authorization': 'Bearer ' + access_token
-        },
-      })
-      .then(res => {
-        set_tracks(res.data.tracks.items);
-      })
-    } catch (err) {
-      console.error(err);
-    } finally {
-      console.log(tracks);
-    }
-  }
-
-  function getHashParams() {
-    let hashParams = {};
-    let e, r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-      hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
-  }
+  var spotify_url = 'https://accounts.spotify.com/authorize';
+      spotify_url += '?response_type=token';
+      spotify_url += '&client_id=' + encodeURIComponent(client_id);
+      spotify_url += '&scope=' + encodeURIComponent(scope);
+      spotify_url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+      spotify_url += '&state' + encodeURIComponent(state);
 
   useEffect(() => {
-    let params = getHashParams()
-    let token = params.access_token;
-    set_access_token(token);
-  });
+    const queryString = new URL(window.location.href.replace("#", "?"))
+      .searchParams;
+    const Accesstoken = queryString.get("access_token");
+    setAccessToken(Accesstoken);
+  }, []);
+
+  useEffect(() => {
+    const handleCombineTrack = tracks.map((track) => ({
+      ...track,
+      select : selected.find((data) => data === track.uri),
+    }));
+    setCombineTrack(handleCombineTrack);
+  }, [tracks, selected]);
+
+  const getTracks = async () => {
+    await axios
+    .get(
+      `https://api.spotify.com/v1/search?q=${search}&type=track&access_token=${accessToken}`
+    )
+    .then((res) => {
+      set_tracks(res.data.tracks.items);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+  const Selecthandler = (uri) => {
+    const onSelected = selected.find((track) => track === uri);
+    onSelected ?
+    setSelected(selected.filter((track) => track !== uri))
+    : setSelected ([...selected, uri]);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Create Playlist</h1>
-      </header>  
-
-      {(!access_token) && (
-        <LoginButton/>
+        <h1>Spotify</h1>
+      </header>
+      {(!accessToken) && (
+        <a href={spotify_url} className="login">Login Spotify</a>
       )}
 
-      {(access_token) && (
-        <Form/>
-      )}
-
+      {(accessToken) && (
+        <div className="search-bar">
+          <input value={search} onChange={e => set_search(e.target.value)} placeholder="Search" />
+          <button onClick={getTracks} className="button-login">Search</button>
+        </div>
+      )}  
       <div>
-        {tracks.map((item) => {
+        {combineTrack.map((track) => {
+          const { name, album, select, uri} = track;
           return (
-            <Song key={item.id} 
-            image={item.album.images[1].url} 
-            title={item.name} 
-            album={item.album.name} 
-            artist={item.album.artists[0].name}/>
+            <Song key={uri} 
+            image={album.images[1].url} 
+            title={name} 
+            album={album.name} 
+            artist={album.artists[0].name}
+            Selecthandler={Selecthandler}
+            uri={uri}
+            select={select}
+            />
           );
         })}
       </div>
-
     </div>
   );
 }
--- src/components/song.js
+++ src/components/song.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const Song = ({image, title, artist, album}) => {
-    return <div className="Song-playlist">
-        <img src={image} alt="album-cover" className="image-album" />
-        <p>Title: {title} </p>
-        <p>Artist: {artist} </p>
-        <p>Album: {album}</p>
-        <button>Select</button>
-    </div>
+const Song = ({image, title, artist, album, Selecthandler, select, uri}) => {
+
+    return <table className="Song">
+        <tr>
+            <td className='img'><img src={image} alt="album-cover" className="image-album" /></td>
+            <td className='detail-song'>
+                <p>{title} </p>
+                <p>{artist} </p>
+                <p>{album}</p>
+                <button onClick={() => {Selecthandler(uri)}}>{select ? 'Deselect' : 'Select'}</button>
+            </td>
+            <td></td>
+        </tr>
+    </table>
 }
 
 export default Song;
